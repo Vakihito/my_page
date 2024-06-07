239af2f981df9b8d736abb4cb3eca6ae48fc3d8e
Victor Akihito Kamada Tomita <40372914+Vakihito@users.noreply.github.com>
2024-06-07 22:16:35 +0000

    create weaks and dates

--- backend/src/goals/model/goals.py
+++ backend/src/goals/model/goals.py
@@ -13,4 +13,9 @@ class GoalsModel(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, index=True, nullable=False)
     title = Column(String, nullable=False)
+    todo = Column(String, nullable=False)
+    nottodo = Column(String, nullable=False)
+    start_date = Column(DateTime, server_default=text("NOW()"))
+    end_date = Column(DateTime, server_default=text("NOW()"))
+    data_format = Column(String, server_default="weaks")
     created_at = Column(DateTime, server_default=text("NOW()"))
--- backend/src/main/settings.py
+++ backend/src/main/settings.py
@@ -4,10 +4,14 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql://postgres:postgres@postgres:5432/postgres"
-    TEST_DATABASE_URL: str = "postgresql://postgres:postgres@postgres:5432/postgres_test"
+    TEST_DATABASE_URL: str = (
+        "postgresql://postgres:postgres@postgres:5432/postgres_test"
+    )
     LOG_LEVEL: str = "INFO"
     LOG_LEVEL_DB: str = "ERROR"
     TESTING: bool = False
+    CHOKIDAR_USEPOLLING: bool = True
+    FAST_REFRESH: bool = True
 
     class Config:
         env_file = ".env"
